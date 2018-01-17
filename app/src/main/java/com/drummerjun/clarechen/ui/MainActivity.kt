8e178dd8cc0a70be4afb7d59b91ae294429c3c8d
junyen.huang <junyen.huang@ehsn.com.tw>
2018-01-17 14:06:18 +0800

    Guillotine Menu selection & display

--- app/src/main/java/com/drummerjun/clarechen/ui/MainActivity.kt
+++ app/src/main/java/com/drummerjun/clarechen/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.drummerjun.clarechen.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.util.Log
@@ -15,11 +18,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.guillotine.*
 import com.yalantis.guillotine.animation.GuillotineAnimation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
     private val TAG = MainActivity::class.simpleName
     private lateinit var staggeredLayoutManager: StaggeredGridLayoutManager
     private val ccapp = CCApp.instance
     private lateinit var adapter: ProductAdapter
+    private lateinit var guillotineAnimation: GuillotineAnimation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,30 +37,105 @@ class MainActivity : AppCompatActivity() {
         val guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null)
         root.addView(guillotineMenu)
 
-        GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), content_hamburger)
+        guillotineAnimation = GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), content_hamburger)
                 .setStartDelay(250)
                 .setActionBarViewForAnimation(toolbar as View)
                 .setClosedOnStart(true)
                 .build()
 
-        group_all.setOnClickListener { Toast.makeText(this, "all", Toast.LENGTH_SHORT).show() }
-        group_boys.setOnClickListener { Toast.makeText(this, "boys", Toast.LENGTH_SHORT).show() }
-        group_girls.setOnClickListener { Toast.makeText(this, "girls", Toast.LENGTH_SHORT).show() }
+        group_all.setOnClickListener {
+            PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt(Constants.KEY_ACTIVE_CATE, 0).apply()
+            guillotineAnimation.close()
+//            adapter.filterByCategory(0)
+//            productlistview.adapter.notifyDataSetChanged()
+            retrieveData()
+        }
+
+        group_girls.setOnClickListener {
+            PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt(Constants.KEY_ACTIVE_CATE, 1).apply()
+            guillotineAnimation.close()
+//            adapter.filterByCategory(1)
+//            productlistview.adapter.notifyDataSetChanged()
+            retrieveData()
+        }
+
+        group_boys.setOnClickListener {
+            PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt(Constants.KEY_ACTIVE_CATE, 2).apply()
+            guillotineAnimation.close()
+//            adapter.filterByCategory(2)
+//            productlistview.adapter.notifyDataSetChanged()
+            retrieveData()
+        }
 
         staggeredLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         productlistview.layoutManager = staggeredLayoutManager
 
-        ccapp.getDb().collection(Constants.COLLECTION_PRODUCT).get()
-                .addOnCompleteListener { task ->
-                    if(task.isSuccessful) {
-                        Log.d(TAG, "success! task=" + task.result)
-                        val products = ArrayList<Product>()
-                        task.result.mapTo(products) { it.toObject(Product::class.java) }
-                        adapter = ProductAdapter(products)
-                        productlistview.adapter = adapter
-                    } else {
-                        Log.e(TAG, "db fail!!!!")
+        swipe_container.setOnRefreshListener(this)
+        swipe_container.setColorSchemeResources(
+                R.color.colorPrimary,
+                android.R.color.white,
+                R.color.colorAccent,
+                R.color.colorPrimaryDark
+        )
+        swipe_container.post({
+            retrieveData()
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        var currentCate = PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt(Constants.KEY_ACTIVE_CATE, 0)
+        retrieveData()
+    }
+
+    override fun onRefresh() {
+        retrieveData()
+    }
+
+    private fun retrieveData() {
+        val cate = PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt(Constants.KEY_ACTIVE_CATE, 0)
+        swipe_container?.isRefreshing = true
+
+        if(cate == 0) {
+            ccapp.getDb().collection(Constants.COLLECTION_PRODUCT).get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "success! task=" + task.result)
+                            val products = ArrayList<Product>()
+                            task.result.mapTo(products) { it.toObject(Product::class.java) }
+                            if (productlistview.adapter == null) {
+                                adapter = ProductAdapter(products)
+                                productlistview.adapter = adapter
+                            } else {
+                                adapter.overrideData(products)
+                                productlistview.adapter.notifyDataSetChanged()
+                            }
+                        } else {
+                            Log.e(TAG, "db fail!!!!")
+                        }
+                        swipe_container?.isRefreshing = false
                     }
-                }
+        } else {
+            ccapp.getDb().collection(Constants.COLLECTION_PRODUCT)
+                    .whereEqualTo("cate", cate)
+                    .get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "success! task=" + task.result)
+                            val products = ArrayList<Product>()
+                            task.result.mapTo(products) { it.toObject(Product::class.java) }
+                            if (productlistview.adapter == null) {
+                                adapter = ProductAdapter(products)
+                                productlistview.adapter = adapter
+                            } else {
+                                adapter.overrideData(products)
+                                productlistview.adapter.notifyDataSetChanged()
+                            }
+                        } else {
+                            Log.e(TAG, "db fail!!!!")
+                        }
+                        swipe_container?.isRefreshing = false
+                    }
+        }
     }
 }
--- app/src/main/java/com/drummerjun/clarechen/ui/ProductAdapter.kt
+++ app/src/main/java/com/drummerjun/clarechen/ui/ProductAdapter.kt
@@ -2,6 +2,7 @@ package com.drummerjun.clarechen.ui
 
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,15 +17,38 @@ import kotlinx.android.synthetic.main.item_product.view.*
  * Created by drummerjun on 1/13/2018.
  */
 class ProductAdapter (private val products: ArrayList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+    val TAG = ProductAdapter::class.simpleName
+    var filtered: ArrayList<Product> = products.clone() as ArrayList<Product>
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         var inflated = LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
         return ProductViewHolder(inflated)
     }
 
-    override fun getItemCount() = products.size
+    override fun getItemCount() = filtered.size
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        holder.bindItems(products[position])
+        holder.bindItems(filtered[position])
+    }
+
+    fun filterByCategory(cate: Int) {
+        Log.d(TAG, "cate=" + cate)
+        Log.d(TAG, "products size=" + products.size)
+        filtered.clear()
+        if(cate == 0) {
+            filtered.addAll(products)
+        } else {
+            products
+                    .filter { it.cate == cate }
+                    .forEach { filtered.add(it) }
+        }
+    }
+
+    fun overrideData(data: ArrayList<Product>) {
+        products.clear()
+        products.addAll(data)
+        filtered.clear()
+        filtered.addAll(data)
     }
 
     class ProductViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
@@ -37,8 +61,8 @@ class ProductAdapter (private val products: ArrayList<Product>) : RecyclerView.A
         override fun onClick(v: View) {
             val context = itemView.context
             val showPhotoIntent = Intent(context, WebAcivity::class.java)
-            showPhotoIntent.putExtra(Constants.KEY_PRODUCT_LINK, product!!.image[0])
-            context.startActivity(showPhotoIntent)
+//            showPhotoIntent.putExtra(Constants.KEY_PRODUCT_LINK, product!!.link[0])
+//            context.startActivity(showPhotoIntent)
         }
 
         fun bindItems(product: Product) {
